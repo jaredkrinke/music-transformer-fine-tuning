7553d358568782521eed14dc8cc2a4dafaf7b99a
Jared Krinke <jared.krinke@gmail.com>
2022-04-27 21:11:03 -0700

    Data generation

--- 1_midi2tfrecord.bat
+++ 1_midi2tfrecord.bat
@@ -1,1 +1,5 @@
-convert_dir_to_note_sequences --input_dir=midi --output_file=tfr/ns.tfrecord
+REM Note: must run first: pushd magenta && pip install -e .[beam] && popd
+
+convert_dir_to_note_sequences --input_dir=midi --output_file=tmp/score2perf_transfer_learning.tfrecord --recursive
+
+REM TODO: Gather more data for evaluation/test sets and convert them in this script
---
+++ 2_datagen.bat
@@ -0,0 +1,1 @@
+t2t_datagen --data_dir=data --problem=score2perf_transfer_learning --alsologtostderr
