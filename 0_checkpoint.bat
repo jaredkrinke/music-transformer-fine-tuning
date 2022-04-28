414b77bc992bb4eb965e8f252d7869a235300b68
Jared Krinke <jared.krinke@gmail.com>
2022-04-27 19:31:13 -0700

    MIDI conversion

---
+++ 0_checkpoint.bat
@@ -0,0 +1,3 @@
+wget https://storage.googleapis.com/magentadata/models/music_transformer/checkpoints/unconditional_model_16.ckpt.data-00000-of-00001
+wget https://storage.googleapis.com/magentadata/models/music_transformer/checkpoints/unconditional_model_16.ckpt.index
+wget https://storage.googleapis.com/magentadata/models/music_transformer/checkpoints/unconditional_model_16.ckpt.meta
---
+++ 1_midi2tfrecord.bat
@@ -0,0 +1,1 @@
+convert_dir_to_note_sequences --input_dir=midi --output_file=tfr/ns.tfrecord
